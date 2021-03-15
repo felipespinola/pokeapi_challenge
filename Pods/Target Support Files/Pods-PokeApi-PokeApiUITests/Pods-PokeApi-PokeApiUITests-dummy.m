6fbc7f097b12a0ca557a2e1063cce54f4eba75eb
felipespinola <felipespinola93@gmail.com>
2021-03-15 09:25:55 -0300

    remove pods from git
    # Conflicts:
    #	Pods/Pods.xcodeproj/project.pbxproj
    #	Pods/Target Support Files/Pods-PokeApi-PokeApiUITests/Pods-PokeApi-PokeApiUITests-acknowledgements.markdown
    #	Pods/Target Support Files/Pods-PokeApi-PokeApiUITests/Pods-PokeApi-PokeApiUITests-acknowledgements.plist
    #	Pods/Target Support Files/Pods-PokeApi-PokeApiUITests/Pods-PokeApi-PokeApiUITests-frameworks-Debug-input-files.xcfilelist
    #	Pods/Target Support Files/Pods-PokeApi-PokeApiUITests/Pods-PokeApi-PokeApiUITests-frameworks-Debug-output-files.xcfilelist
    #	Pods/Target Support Files/Pods-PokeApi-PokeApiUITests/Pods-PokeApi-PokeApiUITests-frameworks-Release-input-files.xcfilelist
    #	Pods/Target Support Files/Pods-PokeApi-PokeApiUITests/Pods-PokeApi-PokeApiUITests-frameworks-Release-output-files.xcfilelist
    #	Pods/Target Support Files/Pods-PokeApi-PokeApiUITests/Pods-PokeApi-PokeApiUITests-frameworks.sh
    #	Pods/Target Support Files/Pods-PokeApi-PokeApiUITests/Pods-PokeApi-PokeApiUITests.debug.xcconfig
    #	Pods/Target Support Files/Pods-PokeApi-PokeApiUITests/Pods-PokeApi-PokeApiUITests.release.xcconfig
    #	Pods/Target Support Files/Pods-PokeApi/Pods-PokeApi-acknowledgements.markdown
    #	Pods/Target Support Files/Pods-PokeApi/Pods-PokeApi-acknowledgements.plist
    #	Pods/Target Support Files/Pods-PokeApi/Pods-PokeApi-frameworks-Debug-input-files.xcfilelist
    #	Pods/Target Support Files/Pods-PokeApi/Pods-PokeApi-frameworks-Debug-output-files.xcfilelist
    #	Pods/Target Support Files/Pods-PokeApi/Pods-PokeApi-frameworks-Release-input-files.xcfilelist
    #	Pods/Target Support Files/Pods-PokeApi/Pods-PokeApi-frameworks-Release-output-files.xcfilelist
    #	Pods/Target Support Files/Pods-PokeApi/Pods-PokeApi-frameworks.sh
    #	Pods/Target Support Files/Pods-PokeApi/Pods-PokeApi.debug.xcconfig
    #	Pods/Target Support Files/Pods-PokeApi/Pods-PokeApi.release.xcconfig
    #	Pods/Target Support Files/Pods-PokeApiTests/Pods-PokeApiTests.debug.xcconfig
    #	Pods/Target Support Files/Pods-PokeApiTests/Pods-PokeApiTests.release.xcconfig

--- Pods/Target Support Files/Pods-PokeApi-PokeApiUITests/Pods-PokeApi-PokeApiUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PokeApi_PokeApiUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PokeApi_PokeApiUITests
-@end
--- Pods/Target Support Files/Pods-PokeApiTests/Pods-PokeApiTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PokeApiTests : NSObject
-@end
-@implementation PodsDummy_Pods_PokeApiTests
-@end
